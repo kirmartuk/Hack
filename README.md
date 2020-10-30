d7897bce3507b085a5b3b370a5818beea2a1773a
Kirill <kirillmartuk@gmail.com>
2020-10-30 16:12:48 +0700

    add swagger documentation

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
 # Hack
 https://moscow-hackaton.herokuapp.com/
-# 1- Activate virtual env
+# 1 - Activate virtual env
 	source env/bin/activate
 # 2 - Run server.py
 	python3 server.py
-# 3 http://localhost:5000/
+# 3 - Visit http://localhost:5000/
 ***
-## Endpoints
-    GET: '/files' - return all files in server
-    POST: '/files/<file_name>' - updload file to server
-    GET: '/files/<file_name>' - download file from server
+## Api documentation >> http://localhost:5000/apidocs
--- server.py
+++ server.py
@@ -1,8 +1,14 @@
 import os
-from flask import Flask, request, abort, jsonify, send_from_directory
+from flask import Flask, request, abort, jsonify
+from flask import send_from_directory, render_template
+from flask_swagger import swagger
+from flasgger import Swagger
+
 
 UPLOAD_DIRECTORY = os.getcwd() + "/src/"
 app = Flask(__name__)
+swagger = Swagger(app)
+
 
 if not os.path.exists(UPLOAD_DIRECTORY):
     os.makedirs(UPLOAD_DIRECTORY)
@@ -10,7 +16,12 @@ if not os.path.exists(UPLOAD_DIRECTORY):
 
 @app.route("/files")
 def list_files():
-    """Endpoint to list files on the server."""
+    """list files on the server
+    ---
+    responses:
+        '200':
+          description: return all files in directory
+        """
     files = []
     for filename in os.listdir(UPLOAD_DIRECTORY):
         path = os.path.join(UPLOAD_DIRECTORY, filename)
@@ -21,13 +32,33 @@ def list_files():
 
 @app.route("/files/<path:path>")
 def get_file(path):
-    """Download a file."""
+    """Download a file.
+    ---
+    parameters:
+         - in: path
+           name: filename
+           type: string
+           required: true
+    responses:
+        '200':
+          description: file downloaded
+        """
     return send_from_directory(UPLOAD_DIRECTORY, path, as_attachment=True)
 
 
 @app.route("/files/<filename>", methods=["POST"])
 def post_file(filename):
-    """Upload a file."""
+    """Upload a file
+     ---
+    parameters:
+         - in: path
+           name: filename
+           type: string
+           required: true
+    responses:
+         200:
+           description: return file
+        """
 
     if "/" in filename:
         # Return 400 BAD REQUEST
