ace36f73bd8706197ad2055696784ca04ea1bd07
walpurgisnatch <Kawemirs@gmail.com>
2020-10-31 20:54:01 +0700

    ebuchiy regexp

--- app/api/shelters.py
+++ app/api/shelters.py
@@ -1,6 +1,6 @@
 from flask import jsonify, request
 from app import db
-from app.models import Shelter, Animal
+from app.models import Shelter
 from app.api import bp
 
 @bp.route('/shelters', methods=['GET'])
