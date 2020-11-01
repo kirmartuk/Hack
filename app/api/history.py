9850d7b634770d5f2a5a5323313b0f3713a34069
Kawemir <Kawemirs@gmail.com>
2020-11-01 12:49:20 +0700

    pizda

--- app/api/history.py
+++ app/api/history.py
@@ -1,12 +1,15 @@
-from flask import jsonify, request
+from flask import jsonify, request, Response
 from app import db
 from app.models import History
 from app.api import bp
 
 @bp.route('/history/<int:id>', methods=['GET'])
-def get_events_for_animal(id):    
-    return jsonify(History.get_events_for(id)))
-    
+def get_events_for_animal(id):
+    try:
+        return jsonify(History.get_events_for(id))
+    except:
+        e = {}
+        return jsonify(e)
 
 @bp.route('/history', methods=['POST'])
 def add_event():
